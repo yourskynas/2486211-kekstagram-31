00cf4db3ece4a10c329944604ff534fc50df9db0
yourskynas <nastiazaharova69@gmail.com>
2024-02-22 08:32:02 +0300

    сделан рефакторинг и написан jsDoc для функции

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,47 @@
 /* eslint-disable no-unused-vars */
-const checkLength = (string, length) => {
-  if (string.length <= length) {
-    return true;
-  }
 
-  return false;
-
-};
+/**
+ * Функция для проверки длины строки
+ * @param {string} string - строка, которую нужно проверить
+ * @param {number} length - максимальная длина
+ * @returns {boolean} - true, если строка меньше или равна указанной длине, false, если строка длиннее
+ */
+function checkLength(string, length) {
+  return string.length <= length;
+}
 
 const checkPalindrome = (string) => {
-  const stringReplace = string.replaceAll(' ', '');
-  const newString = stringReplace.toUpperCase();
+  const newString = string.replaceAll(' ', '').toUpperCase();
   let newString2 = '';
 
-  for(let i = newString.length - 1; i >= 0; i--) {
+  for (let i = newString.length - 1; i >= 0; i--) {
     newString2 += newString[i];
   }
 
-  if(newString === newString2) {
+  if (newString === newString2) {
     return true;
   }
 
   return false;
 };
+
 
 const findNumber = (string) => {
   const typeString = string.toString();
   let newString = '';
   let numbers = '';
 
-  for(let i = 0; i <= typeString.length; i++) {
+  for (let i = 0; i <= typeString.length; i++) {
     newString += parseInt(typeString[i], 10);
   }
 
-  for(let j = 0; j <= newString.length; j++) {
+  for (let j = 0; j <= newString.length; j++) {
     if (!isNaN(newString[j])) {
       numbers += newString[j];
     }
   }
 
-  if(numbers === '') {
+  if (numbers === '') {
     return NaN;
   }
 
