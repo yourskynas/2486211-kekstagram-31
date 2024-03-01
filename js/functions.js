eb6dbc5c4c2bb0f4f0ef4481e92345194014cc20
yourskynas <nastiazaharova69@gmail.com>
2024-03-01 15:53:00 +0300

    Дополнтельное задание - добавлена ф-ия isMeetingToday

--- js/functions.js
+++ js/functions.js
@@ -48,3 +48,43 @@ const findNumber = (string) => {
   return numbers;
 
 };
+
+/**
+ * Функция, принимающая время начала и конца рабочего дня,
+ * время старта и продолжительность встречи в минутах
+ * и возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит
+ *
+ * @param {string} startWork время начала рабочего дня
+ * @param {string} endWork время конца рабочего дня
+ * @param {string} startMeeting время начала встречи
+ * @param {number} lengthMeeting продолжительность встречи в минутах
+ * @return {boolean} возвращает true, если встреча не выходит за рамки рабочего дня, и false, если выходит
+ */
+
+const isMeetingToday = (startWork, endWork, startMeeting, lengthMeeting) => {
+  const startWorktime = startWork.split(':');
+  const endWorktime = endWork.split(':');
+  const startMeetingTime = startMeeting.split(':');
+
+  /**
+   * Функция, которая конвертирует часы в минуты
+   * @param { Array<string> } time массив строк
+   * @return {number} время в минутах
+   */
+  const getInMinutes = (time) => {
+    const result = +time[0] * 60 + +time[1];
+    return result;
+  };
+
+  const startWorkInMinutes = getInMinutes(startWorktime);
+  const endWorkInMinutes = getInMinutes(endWorktime);
+  const endMeetingInMinutes = getInMinutes(startMeetingTime) + lengthMeeting;
+
+  return startWorkInMinutes <= endMeetingInMinutes && endMeetingInMinutes <= endWorkInMinutes;
+}
+
+isMeetingToday('08:00', '17:30', '14:00', 90); // true
+isMeetingToday('8:0', '10:0', '8:0', 120);     // true
+isMeetingToday('08:00', '14:30', '14:00', 90); // false
+isMeetingToday('14:00', '17:30', '08:0', 90);  // false
+isMeetingToday('8:00', '17:30', '08:00', 900); // false
