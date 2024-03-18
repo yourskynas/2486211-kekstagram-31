9750a63c5e9051c3c81b7c2f728bfec75e0417d6
yourskynas <nastiazaharova69@gmail.com>
2024-03-18 15:24:59 +0300

    Создан модуль для отрисовки полноразмерного фото и комментариев

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 import { createPhotos, COUNT_OBJECTS } from './mocks/data.js';
 import { renderPhotos } from './render-photos.js';
+import { prewiewListeners } from './render-full-photo.js';
 
-renderPhotos(createPhotos(COUNT_OBJECTS));
+const createdPhotos = createPhotos(COUNT_OBJECTS);
+renderPhotos(createdPhotos);
+// prewiewListeners(createdPhotos);
+prewiewListeners();
--- js/render-photos.js
+++ js/render-photos.js
@@ -1,41 +1,21 @@
+// import { renderComment } from './render-full-photo.js';
+
 const renderPhotos = (photosData) => {
 
-  const photosElement = document.querySelector('.pictures');
+  const photosEl = document.querySelector('.pictures');
   const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
-  const randomPhotoSection = document.createDocumentFragment();
+  const photoFragment = document.createDocumentFragment();
 
   photosData.forEach(({url, description, likes, comments}) => {
-    const photoElement = photoTemplate.cloneNode(true);
-    const imageElement = photoElement.querySelector('img');
-    imageElement.src = url;
-    imageElement.alt = description;
-    photoElement.querySelector('.picture__likes').textContent = likes;
-    photoElement.querySelector('.picture__comments').textContent = comments.length;
-    randomPhotoSection.append(photoElement);
+    const photoEl = photoTemplate.cloneNode(true);
+    const imageEl = photoEl.querySelector('img');
+    imageEl.src = url;
+    imageEl.alt = description;
+    photoEl.querySelector('.picture__likes').textContent = likes;
+    photoEl.querySelector('.picture__comments').textContent = comments.length;
+    photoFragment.append(photoEl);
   });
-
-  photosElement.append(randomPhotoSection);
-
-  //перенести в другой модуль
-  const fullPhotoElement = document.querySelector('.big-picture');
-  const minipictures = photosElement.querySelectorAll('.picture');
-  const fullImgEl = fullPhotoElement.querySelector('img');
-  const likesFull = fullPhotoElement.querySelector('.likes-count');
-  const numCommentsFull = fullPhotoElement.querySelector('.social__comment-total-count');
-
-  for (const minipicture of minipictures) {
-    const minipictureSrc = minipicture.querySelector('img');
-    const minipictureLikes = minipicture.querySelector('.picture__likes');
-    const minipictureNumComments = minipicture.querySelector('.picture__comments');
-
-    minipicture.addEventListener('click', () => {
-      fullPhotoElement.classList.remove('hidden');
-      fullImgEl.src = minipictureSrc.src;
-      likesFull.textContent = minipictureLikes.textContent;
-      numCommentsFull.textContent = minipictureNumComments.textContent;
-    });
-  }
-
+  photosEl.append(photoFragment);
 };
 
 export {renderPhotos};
