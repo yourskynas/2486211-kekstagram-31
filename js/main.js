2d9c138527fd621bb4b41b2eb0f515e4f49ddc1c
yourskynas <nastiazaharova69@gmail.com>
2024-04-03 15:47:34 +0300

    Сделан отдельный модуль для эффектов

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import { createPhotos, COUNT_OBJECTS } from './mocks/data.js';
 import { renderPhotos } from './render-photos.js';
 import { addListeners } from './full-photo.js';
-import { initUploadModal } from './photo-upload.js';
+import { initUploadPopap } from './photo-upload.js';
 
 const createdPhotos = createPhotos(COUNT_OBJECTS);
 renderPhotos(createdPhotos);
 addListeners(createdPhotos);
-initUploadModal();
+initUploadPopap();
