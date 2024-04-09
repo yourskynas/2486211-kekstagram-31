cc65a5c9a6792db501a0cbffb74c99b34d3cef95
yourskynas <nastiazaharova69@gmail.com>
2024-04-09 04:32:52 +0300

    удалены файлы с моками

--- js/mocks/util.js
+++
@@ -1,8 +0,0 @@
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-export {getRandomInteger};
