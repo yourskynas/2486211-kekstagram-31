9547875259da274be7c3aa00335d9664743da9d6
yourskynas <nastiazaharova69@gmail.com>
2024-04-09 23:10:47 +0300

    добавлено форматирование для слайдера

--- js/effects.js
+++ js/effects.js
@@ -95,6 +95,17 @@ noUiSlider.create(effectSliderEl, {
   start: 100,
   step: 1,
   connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
 });
 
 const addFilter = (filter, value) => {
